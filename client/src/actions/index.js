dd5dd67f08626cca151db9c9ce365f83292834b9
github username <github email address>
2019-11-07 11:42:06 -0500

    getTracks and getTrack action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,19 +1,43 @@
 const baseUrl = 'http://localhost:3001'
 
-export const addProject = ( project, history ) => {
+export const addTrack = ( track, history ) => {
     return dispatch => {
-      return fetch(baseUrl + '/projects', {
+      return fetch(baseUrl + '/tracks', {
         method: "POST",
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ project })
+        body: JSON.stringify({ track })
       })
       .then(resp => resp.json())
-      .then(project => {
-        dispatch({ type: "ADD_project", project })
-        history.push('/projects')
+      .then(track => {
+        dispatch({ type: "ADD_TRACK", track })
+        history.push('/tracks')
       })
     }
+  }
+
+  export const getTracks = () => {
+    return dispatch => {
+      dispatch({ type: "LOADING_TRACKS" })
+      return fetch(baseUrl + '/tracks')
+        .then(resp => resp.json())
+        .then(tracks => {
+          return dispatch({ type: 'GET_TRACKS', tracks })
+        })
+    }
+  }
+  
+  export const getTrack = id => {
+    return dispatch => {
+      dispatch({ type: "LOADING_TRACKS" });
+      return fetch(baseUrl + '/tracks/' + id)
+        .then(resp => resp.json())
+        .then( track => dispatch({ type: "GET_TRACK", track }))
+    }
+  }
+  
+  export const removeTrack = (id, history) => {
+  
   }
