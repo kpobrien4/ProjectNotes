f12a2a3afedbd1c71efa4a95eb83b88d278de0e5
github username <github email address>
2019-11-07 11:34:04 -0500

    Add project action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,19 @@
 const baseUrl = 'http://localhost:3001'
 
-
+export const addProject = ( project, history ) => {
+    return dispatch => {
+      return fetch(baseUrl + '/projects', {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ project })
+      })
+      .then(resp => resp.json())
+      .then(project => {
+        dispatch({ type: "ADD_project", project })
+        history.push('/projects')
+      })
+    }
+  }
---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import tracksReducer from './tracksReducer';
+
+export default combineReducers({
+  tracksReducer
+})
