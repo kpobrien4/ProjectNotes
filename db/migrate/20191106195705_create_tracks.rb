e722f5e7c815cfe71c96b3eb96268a32c26c2c41
github username <github email address>
2019-11-09 09:41:09 -0500

    reformatted db

--- db/migrate/20191106195705_create_tracks.rb
+++ db/migrate/20191106195705_create_tracks.rb
@@ -2,7 +2,7 @@ class CreateTracks < ActiveRecord::Migration[5.2]
   def change
     create_table :tracks do |t|
       t.string :title
-      t.text :notes
+      t.string :notes
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_195705) do
 
   create_table "tracks", force: :cascade do |t|
     t.string "title"
-    t.text "notes"
+    t.string "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
