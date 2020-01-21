8f6fe9eb7afaf3c00338df31d1facbf9e8fa1a69
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-21 09:03:17 -0500

    hash

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Form from './containers/Form';
