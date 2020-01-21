4b2b44fbf38d59e96ff940248c454c012be14ff4
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-21 10:11:59 -0500

    beginning formatting

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export class Home extends Component {
   render() {
     return (
       <div>
+        <Link to="/tracks/new"><button class="right waves-effect waves-light btn-large light-blue darken-1">Add a Track</button></Link>
+        <br></br>
         <h1 className="center">Welcome to TrackNotes!</h1>
         <h3 class="center grey-text">Keep your projects on track</h3>
       </div>
